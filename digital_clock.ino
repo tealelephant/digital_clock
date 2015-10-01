2648b2c7db19b1a8065812c73d32f64702da825c
tealelephant <hannahcukierman@gmail.com>
2015-10-01 10:46:59 -0500

    wrote code
    we wired the digital display and wrote the code that we needed to write

--- digital_clock.ino
+++ digital_clock.ino
@@ -5,13 +5,56 @@
 
 #define d1 13
 #define a 2
+#define f 7
+#define d2 12
+#define d3 11
+#define b 3
+#define e 6
+#define d 5
+#define dp 9
+#define c 4
+#define g 8 
+#define d4 10 
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(d1, OUTPUT);
+  pinMode(a, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(d2, OUTPUT);
+  pinMode(d3, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(dp, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(d4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(d1, HIGH);
+  digitalWrite(d1, LOW);
+  digitalWrite(a, HIGH);
+  digitalWrite(a, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(f, LOW);
+  digitalWrite(d2, HIGH);
+  digitalWrite(d2, LOW);
+  digitalWrite(d3, HIGH);
+  digitalWrite(d3, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(b, LOW);
+  digitalWrite(e, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(d, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(dp, HIGH);
+  digitalWrite(dp, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(c, LOW);
+  digitalWrite(g, HIGH);
+  digitalWrite(g, LOW);
+  digitalWrite
 }
